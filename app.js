7887c654ecdd63f468eaa13e3a692670c38352f2
muhammadhammadahmed03122169468 <hammad03122169468@gmail.com>
2023-10-22 17:03:50 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+        //   Even & ODD Number
+for (var i =2; i<=50; i+=2)
+console.log(i)
+
+for (var i =1; i<=50; i+=2)
+console.log(i)
+
+
+
+
+
+
+
+            //   Student Data
+var student = [
+    {
+        Name: "Hammad Ahmed",
+        Age: 19,
+        Class: "Web and development"
+    }, 
+    {
+        Name: "Rehman",
+        Age: 23,
+        Class: "Web and development"
+    },
+    {
+        Name: "Ammad",
+        Age: 25,
+        Class: "Web and development"
+    },
+    {
+        Name: "Yousuf",
+        Age: 23,
+        Class: "Web and development"
+    },
+    {
+        Name: "Ahmed",
+        Age: 19,
+        Class: "Web and development"
+    }
+];
+for (var i = 0; i <=student.length; i++){
+  document.write("<br>",student[i].Name)
+  document.write("<br>",student[i].Age)
+  document.write("<br>",student[i].Class)
+  document.write("<br>")
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var a = 50;
+var b = 5;
+var num = a
+a = b
+b = num
+
+console.log(a);
+console.log(b);
+
